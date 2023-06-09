863f8d005a6a9fb175ab29cf9b346157ebf3cbf0
a-manri <alvaro_manriquez@outlook.com>
2023-06-09 16:55:36 -0400

    update con cambios de nombres internos

--- tests/testserial.py
+++ tests/testserial.py
@@ -5,19 +5,19 @@
 import serial,time
 if __name__ == '__main__':
     print('Running. Press CTRL-C to exit.')
-    with serial.Serial("/dev/ttyUSB0", 9600, timeout=1) as arduino:
+    with serial.Serial("/dev/ttyUSB0", 9600, timeout=1) as esp:
         time.sleep(0.1) #wait for serial to open
-        if arduino.isOpen():
-            print("{} connected!".format(arduino.port))
+        if esp.isOpen():
+            print("{} connected!".format(esp.port))
             try:
                 while True:
                     #cmd=input("Enter command (data,led0 or led1): ")
-                    #arduino.write(cmd.encode())
-                    #time.sleep(0.1) #wait for arduino to answer
+                    #esp.write(cmd.encode())
+                    #time.sleep(0.1) #wait for esp to answer
                     
-                    while arduino.inWaiting()==0: pass
-                    if  arduino.inWaiting()>0: 
-                        answer=str(arduino.readline())
+                    while esp.inWaiting()==0: pass
+                    if  esp.inWaiting()>0: 
+                        answer=str(esp.readline())
                         print("---> {}".format(answer))
                         #if cmd=="data":
                         #    dataList=answer.split("x")
@@ -25,7 +25,7 @@ if __name__ == '__main__':
                         #    print("Analog input A0 : {}".format(dataList[1]))
                         #    print("Analog input A1: {}".format(dataList[2]))
                             
-                        arduino.flushInput() #remove data after reading
+                        esp.flushInput() #remove data after reading
                             
             except KeyboardInterrupt:
-                print("KeyboardInterrupt has been caught.")
+                print("KeyboardInterrupt")
